7f42748a6b98e5b3c6833d99d1a4e07bc1aa35f0
kazakos13 <it22033@hua.gr>
2023-05-06 17:05:48 +0300

    added more db features

--- MOCK_DATA.sql
+++ MOCK_DATA.sql
@@ -8,28 +8,28 @@ insert into Users (username, first_name, last_name, phone) values ('dtarply6', '
 insert into Users (username, first_name, last_name, phone) values ('apepperd7', 'Arron', 'Pepperd', '979-914-2621');
 insert into Users (username, first_name, last_name, phone) values ('dberrigan8', 'Danielle', 'Berrigan', '554-148-7587');
 insert into Users (username, first_name, last_name, phone) values ('hknappitt9', 'Hanan', 'Knappitt', '199-255-3575');
-INSERT INTO Discs (name, creation_date, price, band) VALUES ('Abbey Road', '1969-09-26', 25.99, 'the+beatles');
-INSERT INTO Discs (name, creation_date, price, band) VALUES ('The Game', '1980-06-30', 15.99, 'queen');
-INSERT INTO Discs (name, creation_date, price, band) VALUES ('Back in Black', '1980-07-25', 19.99, 'acdc');
-INSERT INTO Discs (name, creation_date, price, band) VALUES ('The Joshua Tree', '1987-03-09', 22.99, 'u2');
-INSERT INTO Discs (name, creation_date, price, band) VALUES ('Parachutes', '2000-07-10', 12.99, 'coldplay');
-INSERT INTO Discs (name, creation_date, price, band) VALUES ('Help!', '1965-08-06', 23.99, 'the+beatles');
-INSERT INTO Discs (name, creation_date, price, band) VALUES ('A Night at the Opera', '1975-11-21', 17.99, 'queen');
-INSERT INTO Discs (name, creation_date, price, band) VALUES ('Highway to Hell', '1979-07-27', 21.99, 'acdc');
-INSERT INTO Discs (name, creation_date, price, band) VALUES ('The Unforgettable Fire', '1984-10-01', 18.99, 'u2');
-INSERT INTO Discs (name, creation_date, price, band) VALUES ('Viva la Vida or Death and All His Friends', '2008-06-12', 14.99, 'coldplay');
-INSERT INTO Bands (name, creation_date)
-VALUES ('the+beatles', '1960-08-01');
+INSERT INTO Discs (name, band) VALUES ('Abbey Road', 'the+beatles');
+INSERT INTO Discs (name, band) VALUES ('The Game', 'queen');
+INSERT INTO Discs (name, band) VALUES ('Back in Black', 'acdc');
+INSERT INTO Discs (name, band) VALUES ('The Joshua Tree',  'u2');
+INSERT INTO Discs (name, band) VALUES ('Parachutes','coldplay');
+INSERT INTO Discs (name, band) VALUES ('Help!', 'the+beatles');
+INSERT INTO Discs (name, band) VALUES ('A Night at the Opera', 'queen');
+INSERT INTO Discs (name, band) VALUES ('Highway to Hell', 'acdc');
+INSERT INTO Discs (name, band) VALUES ('The Unforgettable Fire','u2');
+INSERT INTO Discs (name, band) VALUES ('Viva la Vida or Death and All His Friends','coldplay');
+INSERT INTO Bands (name)
+VALUES ('the+beatles');
 
-INSERT INTO Bands (name, creation_date)
-VALUES ('queen', '1970-06-27');
+INSERT INTO Bands (name)
+VALUES ('queen');
 
-INSERT INTO Bands (name, creation_date)
-VALUES ('acdc', '1973-11-01');
+INSERT INTO Bands (name)
+VALUES ('acdc');
 
-INSERT INTO Bands (name, creation_date)
-VALUES ('u2', '1976-09-25');
+INSERT INTO Bands (name)
+VALUES ('u2');
 
-INSERT INTO Bands (name, creation_date)
-VALUES ('coldplay', '1996-01-16');
+INSERT INTO Bands (name)
+VALUES ('coldplay');
 
